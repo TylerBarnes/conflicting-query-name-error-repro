4dff154e41f947f79043e6b96e0f8f2a55e1adfe
Tyler Barnes <tylerdbarnes@gmail.com>
2020-04-29 13:12:17 -0700

    cause unknown fragment error due to conflicting query name

--- src/templates/single/Post.js
+++ src/templates/single/Post.js
@@ -5,15 +5,10 @@ import BlogPost from "../../components/template-parts/blog-post"
 export default ({ data }) => <BlogPost data={data} />
 
 export const query = graphql`
-  query post($id: String!, $nextPage: String, $previousPage: String) {
+  query page($id: String!, $nextPage: String, $previousPage: String) {
     page: wpPost(id: { eq: $id }) {
       title
       content
-      featuredImage {
-        remoteFile {
-          ...HeroImage
-        }
-      }
     }
 
     nextPage: wpPost(id: { eq: $nextPage }) {
